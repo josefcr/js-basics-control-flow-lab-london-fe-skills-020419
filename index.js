9ed01874402a7fefeac5819b3f73d734ef6a8440
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-18 18:24:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else { 
+    return 'No can do.'
+  }
+  
+}
+
+function ternaryCheckCity(city){
+  return city == 'NYC' ? "Ok, sounds good.":'No go.';
+   
+}
+
+function switchOnCharmFromTip(tip){
+  
+  switch (charm)
+  case generous : "Thank you so much";
+  break
+  
+}
+
+
+  /*describe('switchOnCharmFromTip()', function () {
+    it('should return "Thank you so much." if the tip is generous', function () {
+      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+    });
+
+    it('should return "Thank you." if the tip is not as generous', function () {
+      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+    });
+
+    it('should return "Bye." if anything else', function () {
+      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+    });
+  });
